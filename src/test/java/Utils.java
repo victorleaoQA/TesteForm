629d1fc045a3ef04f249ccd08411adadbb97f9da
victorleaoQA <leaovic@icloud.com>
2022-09-06 23:57:45 -0300

    TesteSeleniumMavem

---
+++ src/test/java/Utils.java
@@ -0,0 +1,6 @@
+public class Utils {
+    final static String BASE_URL = "https://formy-project.herokuapp.com/form";
+    final static String CHROME_DRIVER_LOCATION = "chromedriver";
+    final static String EDGE_DRIVER_LOCATION = "msedgedriver";
+    final static String FIREFOX_DRIVER_LOCATION = "geckodriver";
+}
