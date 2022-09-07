c1f6bfa79c3dc4ee0e9a6591e9d1c11fd923919a
victorleaoQA <leaovic@icloud.com>
2022-09-07 00:37:58 -0300

    TesteSeleniumMavem

--- src/test/java/TestPlan.java
+++ src/test/java/TestPlan.java
@@ -1,5 +1,5 @@
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -7,15 +7,15 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 public class TestPlan {
-    private static final WebDriver driver = new ChromeDriver();
+    private static final WebDriver driver = new FirefoxDriver();
 
     @BeforeSuite
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", Utils.CHROME_DRIVER_LOCATION);
+        System.setProperty("webdriver.firefox.driver", Utils.FIREFOX_DRIVER_LOCATION);
     }
 
     @Test(testName = "Submit a WebForm")
-    public static void submitForm() {
+    public static void submitForm() throws InterruptedException {
         driver.get(Utils.BASE_URL);
         driver.getTitle();
         WebForm webForm = new WebForm(driver);
--- src/test/java/WebForm.java
+++ src/test/java/WebForm.java
@@ -3,8 +3,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class WebForm extends PageObject{
-    private final String FIRST_NAME = "First_name";
-    private final String LAST_NAME = "Last_name";
+    private final String FIRST_NAME = "Victor";
+    private final String LAST_NAME = "Leao";
+
+    private final String JOB_TITLE = "Marinheiro";
 
     @FindBy(id = "first-name")
     private WebElement first_name;
@@ -12,8 +14,12 @@ public class WebForm extends PageObject{
     @FindBy(id = "last-name")
     private WebElement last_name;
 
+    @FindBy(id = "job-title")
+    private WebElement job_title;
+
     @FindBy(xpath = "//a[contains(text(),'Submit')]")
     private WebElement submit_button;
+
 
     @FindBy(xpath = "//div[contains(text(),'The form was successfully submitted!')]")
     private WebElement alertSuccess;
@@ -28,6 +34,10 @@ public class WebForm extends PageObject{
 
     public void enterLastName(){
         this.last_name.sendKeys(LAST_NAME);
+    }
+
+    public void enterJobTitle(){
+        this.job_title.sendKeys(JOB_TITLE);
     }
 
     public void pressSubmitButton(){
